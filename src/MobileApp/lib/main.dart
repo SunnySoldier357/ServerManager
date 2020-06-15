fbac5c6e75045e2273a1ca8b0236096bc75aa402
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2020-06-15 08:14:54 +0800

    [MobileApp] Add DI with NavigationService

--- src/MobileApp/lib/main.dart
+++ src/MobileApp/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-void main() => runApp(App());
+import 'package:servermanager/app/locator.dart';
+
+void main() {
+  initLocator();
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
+      navigatorKey: locator<NavigationService>().navigatorKey,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
